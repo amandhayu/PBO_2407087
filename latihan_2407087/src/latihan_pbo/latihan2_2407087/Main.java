03df550519bcfc61321655d99ce8de3d9f3628e8
amandha ayu <amandhaayoe@gmail.com>
2026-02-20 14:43:37 +0700

    commit kedua

---
+++ latihan_2407087/src/latihan_pbo/latihan2_2407087/Main.java
@@ -0,0 +1,13 @@
+package latihan_pbo.latihan2_2407087;
+
+class cafe {
+    String nama;
+    String alamat;
+}
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+}
